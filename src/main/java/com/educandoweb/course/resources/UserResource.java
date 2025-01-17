3f5d77e25d82f18b4ac9bbeb130c270ba4f8ddd6
Thiago <thiagorenatino@gmail.com>
2025-01-17 02:59:23 -0300

    User entity and resource created

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.educandoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.User;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Maria","mariaadad@gmail.com","119898392","12313");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
